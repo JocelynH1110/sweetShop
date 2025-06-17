e06b0553cc78b67a7537691f2e9e4a6dc6dc0d3d
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2025-06-18 00:45:51 +0800

    rourou

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 const isProd = process.env.NODE_ENV === "production";
 
 // https://astro.build/config
 export default defineConfig({
   // Add these two lines; replace `moroz` with your username
   site: isProd ? "https://jocelynH1110.github.io" : undefined,
+
   // Replace `megane` with the name of your project
   base: isProd ? "sweetShop" : undefined,
+
+  integrations: [svelte()],
 });
---
+++ src/components/Footer.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import Counter from "./Counter.svelte";
+
+  interface Props {
+    baseURL: string;
+  }
+
+  const { baseURL }: Props = $props();
+</script>
+
+<footer>
+  <my-container>
+    <Counter />
+    <section>
+      <img src="{baseURL}/images/logo-light.svg" alt="logo" />
+      <p>訂閱你我的甜蜜郵件</p>
+      <input type="email" />
+    </section>
+    <div>
+      <img src="{baseURL}/images/logotype-lg-dark.svg" alt="" />
+      <img src="{baseURL}/images/sm-今天是個吃甜點的好日子.svg" alt="" />
+      <p>07-1234-5678 sweetaste@email.com 800 高雄市新興區幸福路 520 號</p>
+      <img src="{baseURL}/images/ic-line@.svg" alt="line" />
+      <img src="{baseURL}/images/ic-facebook.svg" alt="facebook" />
+      <p>© 2018 Sweetaste* All Rights Reserved</p>
+    </div>
+  </my-container>
+</footer>
+
+<style>
+  section {
+    background: var(--header-fg);
+    color: var(--footer-bg);
+  }
+  img {
+    width: 2.5rem;
+  }
+  div {
+    background: var(--footer-bg);
+  }
+</style>
